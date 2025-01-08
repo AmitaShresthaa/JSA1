01c43ae49ea03b1a7d6cc26f624f22508bdb6c3f
Amita <amitastha0o7@gmail.com>
2025-01-09 01:24:53 +1100

    Done

---
+++ script.js
@@ -0,0 +1,342 @@
+const outputTag =  document.getElementById('output');
+outputTag.innerText = "Introduction to Java Script";
+
+const outputTag1 = document.getElementById('output1');
+outputTag1.innerText = "JavaScript was invented by Brendan Eich in 1995, and became an ECMA standard in 1997. ECMA-262 is the official name of the standard. ECMAScript is the official name of the language.";
+
+const outputTag2 = document.getElementById('output2');
+outputTag2.innerText = "The JavaScript syntax defines two types of values:"
+
+const outputTag3 = document.getElementById('output3');
+outputTag3.innerText = "Fixed values: They are called Literals."
+
+const outputTag4 = document.getElementById('output4');
+outputTag4.innerText = "Variable values: They are called Variables. JavaScript Variables can be declared in 4 ways:"
+
+const outputTag5 = document.getElementById('output5');
+outputTag5.innerText = "JavaScript Variables can be declared in 4 ways:"
+
+const outputTag5i = document.getElementById('output5i');
+outputTag5i.innerText = "Automatically"
+console.log('out=',outputTag5.innerText)
+
+const outputTag5ii = document.getElementById('output5ii');
+outputTag5ii.innerText = "Using Let"
+
+const outputTag5iii = document.getElementById('output5iii');
+outputTag5iii.innerText = "Using Const"
+
+const outputTag5iv = document.getElementById('output5iv');
+outputTag5iv.innerText = "Using Var"
+
+const outputTag6 = document.getElementById('output6');
+outputTag6.innerText = "JavaScript Data Type"
+
+const outputTagrha = document.getElementById('row-Ha');
+outputTagrha.innerText = " Data Type "
+
+const outputTagrhb = document.getElementById('row-Hb');
+outputTagrhb.innerText = " Type of Return Value "
+
+const outputTagrhc= document.getElementById('row-Hc');
+outputTagrhc.innerText = " Example "
+
+const outputTagr11 = document.getElementById('r11');
+outputTagr11.innerText = " Null"
+
+const outputTagr12 = document.getElementById('r12');
+outputTagr12.innerText = " Object "
+
+const outputTagr13= document.getElementById('r13');
+outputTagr13.innerText = " N/A  "
+
+const outputTagr21 = document.getElementById('r21');
+outputTagr21.innerText = " Undefined"
+
+const outputTagr22 = document.getElementById('r22');
+outputTagr22.innerText = " Undefined"
+
+const outputTagr23= document.getElementById('r23');
+outputTagr23.innerText = " N/A  "
+
+const outputTagr31 = document.getElementById('r31');
+outputTagr31.innerText = " Boolean"
+
+const outputTagr32 = document.getElementById('r32');
+outputTagr32.innerText = " boolean"
+
+const outputTagr33= document.getElementById('r33');
+outputTagr33.innerText = " let x = 'true', let y = 'false' "
+
+const outputTagr41 = document.getElementById('r41');
+outputTagr41.innerText = " Number "
+
+const outputTagr42 = document.getElementById('r42');
+outputTagr42.innerText = "number "
+
+const outputTagr43= document.getElementById('r43');
+outputTagr43.innerText = "const x = 222; const y = 22.2222 "
+
+const outputTagr51 = document.getElementById('r51');
+outputTagr51.innerText = " BigInt"
+
+const outputTagr52 = document.getElementById('r52');
+outputTagr52.innerText = " bigInt "
+
+const outputTagr53= document.getElementById('r53');
+outputTagr53.innerText = " const hugeString = BigInt(9007199254740991);"
+
+const outputTagr61 = document.getElementById('r61');
+outputTagr61.innerText = " String "
+
+const outputTagr62 = document.getElementById('r62');
+outputTagr62.innerText = " string "
+
+const outputTagr63= document.getElementById('r63');
+outputTagr63.innerText = " const = 'Hello!'; const = 'fo12348-3920';" 
+
+const outputTagr71 = document.getElementById('r71');
+outputTagr71.innerText = " Symbol "
+
+const outputTagr72 = document.getElementById('r72');
+outputTagr72.innerText = " symbol "
+
+const outputTagr73= document.getElementById('r73');
+outputTagr73.innerText = " const sym2 = Symbol('foo');"
+
+const outputTag6a = document.getElementById('output6a');
+outputTag6a.innerText = "Javascript operators are used to perform different types of mathematical and logical computations."
+
+const outputTag6i = document.getElementById('output6i');
+outputTag6i.innerText = "The Assignment Operator assigns values"
+
+const outputTag6ii = document.getElementById('output6ii');
+outputTag6ii.innerText = "The Addition Operator + adds values"
+
+const outputTag6iii = document.getElementById('output6iii');
+outputTag6iii.innerText = "The Multiplication Operator * multiplies values"
+
+const outputTag6iv = document.getElementById('output6iv');
+outputTag6iv.innerText = "The Comparison Operator > compares values"
+
+const outputTag7 = document.getElementById('output7');
+outputTag7.innerText = "Types of JavaScript Operators"
+
+const outputTag8 = document.getElementById('output8');
+outputTag8.innerText = "There are different types of JavaScript operators:"
+
+const outputTag8i = document.getElementById('output8i');
+outputTag8i.innerText = "Arithmetic Operators"
+
+const outputTag8ii = document.getElementById('output8ii');
+outputTag8ii.innerText = "Assignment Operators"
+
+const outputTag8iii = document.getElementById('output8iii');
+outputTag8iii.innerText = "Comparison Operators"
+
+const outputTag8iv = document.getElementById('output8iv');
+outputTag8iv.innerText = "String Operators"
+
+const outputTag8v = document.getElementById('output8v');
+outputTag8v.innerText = "Logical Operators"
+
+const outputTag8vi = document.getElementById('output8vi');
+outputTag8vi.innerText = "Bitwise Operators"
+
+const outputTag8vii = document.getElementById('output8vii');
+outputTag8vii.innerText = "Ternary Operators"
+
+const outputTag8viii = document.getElementById('output8viii');
+outputTag8viii.innerText = "Type Operators"
+
+const outputTag9 = document.getElementById('output9');
+outputTag9.innerText = "JavaScript Assignment Operators"
+
+const outputTagrh1 = document.getElementById('row-H1');
+outputTagrh1.innerText = " Operator "
+
+const outputTagrh2 = document.getElementById('row-H2');
+outputTagrh2.innerText = " Example "
+
+const outputTagrh3= document.getElementById('row-H3');
+outputTagrh3.innerText = " Same As "
+
+const outputTagr1a = document.getElementById('r1a');
+outputTagr1a.innerText = " = "
+
+const outputTagr1b = document.getElementById('r1b');
+outputTagr1b.innerText = " x = y "
+
+const outputTagr1c= document.getElementById('r1c');
+outputTagr1c.innerText = " x = y  "
+
+const outputTagr2a = document.getElementById('r2a');
+outputTagr2a.innerText = " += "
+
+const outputTagr2b = document.getElementById('r2b');
+outputTagr2b.innerText = " x += y "
+
+const outputTagr2c= document.getElementById('r2c');
+outputTagr2c.innerText = " x = x + y  "
+
+const outputTagr3a = document.getElementById('r3a');
+outputTagr3a.innerText = " -= "
+
+const outputTagr3b = document.getElementById('r3b');
+outputTagr3b.innerText = " x -= y "
+
+const outputTagr3c= document.getElementById('r3c');
+outputTagr3c.innerText = " x = x - y "
+
+const outputTagr4a = document.getElementById('r4a');
+outputTagr4a.innerText = " *= "
+
+const outputTagr4b = document.getElementById('r4b');
+outputTagr4b.innerText = " x *= y "
+
+const outputTagr4c= document.getElementById('r4c');
+outputTagr4c.innerText = " x = x * y "
+
+const outputTagr5a = document.getElementById('r5a');
+outputTagr5a.innerText = " /= "
+
+const outputTagr5b = document.getElementById('r5b');
+outputTagr5b.innerText = " x /= y "
+
+const outputTagr5c= document.getElementById('r5c');
+outputTagr5c.innerText = " x = x / y "
+
+const outputTagr6a = document.getElementById('r6a');
+outputTagr6a.innerText = " %= "
+
+const outputTagr6b = document.getElementById('r6b');
+outputTagr6b.innerText = " x %= y "
+
+const outputTagr6c= document.getElementById('r6c');
+outputTagr6c.innerText = " x = x % y "
+
+const outputTagr7a = document.getElementById('r7a');
+outputTagr7a.innerText = " **= "
+
+const outputTagr7b = document.getElementById('r7b');
+outputTagr7b.innerText = " x **= y "
+
+const outputTagr7c= document.getElementById('r7c');
+outputTagr7c.innerText = " x = x ** y "
+
+const outputTag10 = document.getElementById('output10');
+outputTag10.innerText = "Shift Assignment Operators"
+
+const outputTagrh1a = document.getElementById('row-H1a');
+outputTagrh1a.innerText = " Operator "
+
+const outputTagrh2a = document.getElementById('row-H2a');
+outputTagrh2a.innerText = " Example "
+
+const outputTagrh3a= document.getElementById('row-H3a');
+outputTagrh3a.innerText = " Same As "
+
+const outputTagr1aa = document.getElementById('r1aa');
+outputTagr1aa.innerText = " <<= "
+
+const outputTagr1bb = document.getElementById('r1bb');
+outputTagr1bb.innerText = " x <<= y "
+
+const outputTagr1cc= document.getElementById('r1cc');
+outputTagr1cc.innerText = " x = x << y  "
+
+const outputTagr2aa = document.getElementById('r2aa');
+outputTagr2aa.innerText = " >>= "
+
+const outputTagr2bb = document.getElementById('r2bb');
+outputTagr2bb.innerText = " x >>= y "
+
+const outputTagr2cc= document.getElementById('r2cc');
+outputTagr2cc.innerText = " x = x >> y  "
+
+const outputTagr3aa = document.getElementById('r3aa');
+outputTagr3aa.innerText = " >>>= "
+
+const outputTagr3bb = document.getElementById('r3bb');
+outputTagr3bb.innerText = " x >>>= y "
+
+const outputTagr3cc= document.getElementById('r3cc');
+outputTagr3cc.innerText = " x = x >>> y "
+
+const outputTag11 = document.getElementById('output11');
+outputTag11.innerText = "Bitwise Assignment Operators"
+
+const outputTagrh1b = document.getElementById('row-H1b');
+outputTagrh1b.innerText = " Operator "
+
+const outputTagrh2b = document.getElementById('row-H2b');
+outputTagrh2b.innerText = " Example "
+
+const outputTagrh3b= document.getElementById('row-H3b');
+outputTagrh3b.innerText = " Same As "
+
+const outputTagr1aaa = document.getElementById('r1aaa');
+outputTagr1aaa.innerText = " &="
+
+const outputTagr1bbb = document.getElementById('r1bbb');
+outputTagr1bbb.innerText = " x <&= y "
+
+const outputTagr1ccc= document.getElementById('r1ccc');
+outputTagr1ccc.innerText = " x = x & y  "
+
+const outputTagr2aaa = document.getElementById('r2aaa');
+outputTagr2aaa.innerText = " ^= "
+
+const outputTagr2bbb = document.getElementById('r2bbb');
+outputTagr2bbb.innerText = " x ^= y "
+
+const outputTagr2ccc= document.getElementById('r2ccc');
+outputTagr2ccc.innerText = " x = x ^ y  "
+
+const outputTagr3aaa = document.getElementById('r3aaa');
+outputTagr3aaa.innerText = " |= "
+
+const outputTagr3bbb = document.getElementById('r3bbb');
+outputTagr3bbb.innerText = " x |= y "
+
+const outputTagr3ccc= document.getElementById('r3ccc');
+outputTagr3ccc.innerText = " x = x | y "
+
+const outputTag12 = document.getElementById('output12');
+outputTag12.innerText = "Bitwise Assignment Operators"
+
+const outputTagrh1c = document.getElementById('row-H1c');
+outputTagrh1c.innerText = " Operator "
+
+const outputTagrh2c = document.getElementById('row-H2c');
+outputTagrh2c.innerText = " Example "
+
+const outputTagrh3c= document.getElementById('row-H3c');
+outputTagrh3c.innerText = " Same As "
+
+const outputTagr1aaaa = document.getElementById('r1aaaa');
+outputTagr1aaaa.innerText = " &&="
+
+const outputTagr1bbbb = document.getElementById('r1bbbb');
+outputTagr1bbbb.innerText = " x &&= y "
+
+const outputTagr1cccc= document.getElementById('r1cccc');
+outputTagr1cccc.innerText = " x = x && (x = y) "
+
+const outputTagr2aaaa = document.getElementById('r2aaaa');
+outputTagr2aaaa.innerText = " ||= "
+
+const outputTagr2bbbb = document.getElementById('r2bbbb');
+outputTagr2bbbb.innerText = " x ||= y "
+
+const outputTagr2cccc= document.getElementById('r2cccc');
+outputTagr2cccc.innerText = "x = x || (x = y)  "
+
+const outputTagr3aaaa = document.getElementById('r3aaaa');
+outputTagr3aaaa.innerText = " ??= "
+
+const outputTagr3bbbb = document.getElementById('r3bbbb');
+outputTagr3bbbb.innerText = " x ??= y "
+
+const outputTagr3cccc= document.getElementById('r3cccc');
+outputTagr3cccc.innerText = " x = x ?? (x = y)"
